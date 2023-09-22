79b5ee5c25e06415b2ff8a1e1c36ec24ff18355c
Andy Doro <mail@andydoro.com>
2023-09-22 14:07:28 -0400

    deleted redunandant
    deleted redunandant

--- alphanumeric_holidate/alphanumeric_holidate02/adjustBrightness.ino
+++
@@ -1,8 +0,0 @@
-
-void adjustBrightness(byte theHour) {
-  if (theHour < MORNINGCUTOFF || theHour >= NIGHTCUTOFF) {
-    myAlphanum.brightness(NIGHTBRIGHTNESS);
-  } else {
-    myAlphanum.brightness(DAYBRIGHTNESS);
-  }
-}
